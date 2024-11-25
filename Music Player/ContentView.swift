eea3f348ef04f12ff7538828f110a43850edd4f5
ZiXiang <wzxiang9301@gmail.com>
2024-11-25 15:21:13 +0800

    Initial Commit

---
+++ Music Player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Music Player
+//
+//  Created by 翁子翔 on 2024/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Music PlayerTests/Music_PlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerTests.swift
+//  Music PlayerTests
+//
+//  Created by 翁子翔 on 2024/11/25.
+//
+
+import Testing
+@testable import Music_Player
+
+struct Music_PlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
